5eb2ab872e3166c34ebf0ec6941e65e9903df7fa
Maiko Duarte <mkdu185@gmail.com>
2023-02-26 16:30:41 -0300

    test: ensure DbAddAccount returns an account on sucess

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -89,6 +89,23 @@ describe('DbAddAccount Usecase', () => {
         })
     })
     
+    test('Should return an account on success', async () => {
+        const { sut } = makeSut()
+
+        const accountData = {
+            name: 'valid_name',
+            email: 'valid_email',
+            password: 'valid_password'
+        }
+        const account = await sut.add(accountData)
+        expect(account).toEqual({
+            id: 'valid_id',
+            name: 'valid_name',
+            email: 'valid_email',
+            password: 'hashed_password'
+        })
+    })
+    
 
 
 
