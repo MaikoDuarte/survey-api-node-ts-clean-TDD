6df64fe552cdf9fddc0761994a54f2628a67e47a
Maiko Duarte <mkdu185@gmail.com>
2023-02-26 14:38:19 -0300

    refactor: jest and babel configuration

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,13 @@
-module.export = {
-  roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  coverageDirectory: "coverage",
-  testEnvironment: "jest-environment-node",
-  transform: {
-    '.+\\.ts$': 'ts-jest'
-  }
+module.exports = {
+  preset: 'ts-jest/presets/js-with-ts',
+  testEnvironment: "node",
+  globals: {
+      'ts-jest': {
+          tsconfig: '<rootDir>tsconfig.json',
+      },
+  },
+  transformIgnorePatterns: [
+      "node_modules/(?!troublesome-dependency/.*)",
+  ],
+}
 
-};
